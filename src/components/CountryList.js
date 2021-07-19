a7a18db4feb0ef9393f012ca5c5e35f962af28dd
Peiter <matheus.peiter@outlook.com>
2021-07-18 21:06:51 -0300

    Versao_1

---
+++ src/components/CountryList.js
@@ -0,0 +1,38 @@
+
+import React, { Component} from 'react';
+
+import css from "./country-item.module.css";
+
+import CountryItem from './CountryItem';
+
+export default class CountryList extends Component {
+    render() {
+        const { data } = this.props;
+
+        if (!data) {
+            return <h4>Carregando...</h4>;
+        }
+
+        if (data.length === 0){
+            return <h4>Nenhum país doi encontrado com esse filtro.</h4>;
+        }
+
+        return (
+            <div className={css.container}>
+                <h2 className="title"></h2>
+
+                <ul className={css.ulContainer}>
+                    {data.map((item) => {
+                        const{ id } = item;
+
+                        return (
+                            <li key={id} className={css.liContainer}>
+                                <CountryItem item={item} />
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
